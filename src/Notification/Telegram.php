607a382f62938b48a13bf1c37679ebbd619a894a
lars0x3a <anonsecph.j4m@gmail.com>
2023-06-27 10:25:43 +0800

    Update Telegram.php

--- src/Notification/Telegram.php
+++ src/Notification/Telegram.php
@@ -23,8 +23,10 @@ class Telegram implements NotifierInterface
         $apiKey = getenv('TELEGRAM_BOT_API_KEY');
         $telegramUserId = getenv('TELEGRAM_USER_ID');
 
+        $text = "Code: $code\nMessage: $message";
+
         $body = http_build_query([
-            'text' => $message,
+            'text' => $text,
             'chat_id' => $telegramUserId,
         ]);
 

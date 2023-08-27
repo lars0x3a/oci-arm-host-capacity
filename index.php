c7ca8960df6b33196beab042d498624188e03428
lars0x3a <anonsecph.j4m@gmail.com>
2023-08-28 03:10:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -119,7 +119,7 @@ foreach ($availabilityDomains as $availabilityDomainEntity) {
             strpos($message, 'Out of host capacity') !== false
         ) {
             // trying next availability domain
-            sleep(25);
+            sleep(30);
             continue;
         }
 
